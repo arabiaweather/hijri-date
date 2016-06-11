c237ccdbcba12563ea420e7cf5c6a28d4f5e402b
Osama Gadit <ogadit@gmail.com>
2016-06-11 18:18:16 +0500

    Added language support for english and arabic

---
+++ languages.js
@@ -0,0 +1,52 @@
+var Arabic = {
+  wdNames: [
+    'الأحد',
+    'الاثنين',
+    'الثلاثاء',
+    'الأربعاء',
+    'الخميس',
+    'الجمعة',
+    'السبت'
+  ],
+  MonthNames: [
+    'المحرّم',
+    'صفر',
+    'ربيع الأوّل',
+    'ربيع الآخر',
+	'جمادى الأولى',
+    'جمادى الآخرة',
+    'رجب',
+	'شعبان',
+	'رمضان',
+    'شوّال',
+    'ذو القعدة',
+	'ذو الحجّة'
+  ]
+};
+var English = {
+  wdNames: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ],
+  MonthNames: [
+    'Muharram',
+    'Safar',
+    'Rabi ul-Awal',
+    'Rabi ul-Akhir',
+    'Jamadi ul-Awal',
+    'Jamadi ul-Akhir',
+    'Rajab',
+    'Sha\'ban',
+    'Ramadan',
+    'Shawwal',
+    'Dhul Qad',
+    'Dhil Hajj'
+  ]
+};
+exports.arabic = Arabic;
+exports.english = English;
